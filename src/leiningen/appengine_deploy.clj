7cb702387f6d1dc07c05a7beb02ded749529a134
Constantine Vetoshev <gepardcv@gmail.com>
2010-07-07 23:22:01 -0400

    Added stubs for Leiningen tasks.

---
+++ src/leiningen/appengine_deploy.clj
@@ -0,0 +1,8 @@
+(ns leiningen.appengine-deploy
+  "Deploy the current Google App Engine application.")
+
+
+(defn appengine-deploy [project]
+  (println "deploying project")
+  ;; TODO: Fill this in.
+  )
---
+++ src/leiningen/appengine_new.clj
@@ -0,0 +1,8 @@
+(ns leiningen.appengine-new
+  "Create the skeleton of a Google App Engine application.")
+
+
+(defn appengine-new [project]
+  (println "making new project")
+  ;; TODO: Fill this in.
+  )
