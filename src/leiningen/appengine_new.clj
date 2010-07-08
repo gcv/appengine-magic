1939290f7375ea635e06cb66f9f978fcecb9dbf0
Constantine Vetoshev <gepardcv@gmail.com>
2010-07-08 17:34:58 -0400

    Made appengine-new create a base entry point servlet file.

--- src/leiningen/appengine_new.clj
+++ src/leiningen/appengine_new.clj
@@ -1,6 +1,6 @@
 (ns leiningen.appengine-new
   "Create the skeleton of a Google App Engine application."
-  (:import [java.io File FileInputStream InputStream]
+  (:import [java.io File FileInputStream FileWriter InputStream]
            org.xml.sax.InputSource
            javax.xml.parsers.DocumentBuilderFactory
            [javax.xml.xpath XPathFactory XPathConstants]
@@ -9,17 +9,37 @@
            javax.xml.transform.stream.StreamResult))
 
 
-(defn xpath-replace-all [input out-file xpath-raw-expr new-value]
+(def app-servlet-src
+     (str "(ns %s.%s\n"
+          "  (:gen-class :extends javax.servlet.http.HttpServlet)\n"
+          "  ;; Replace the ... below with the namespace which defines your appengine-app.\n"
+          "  (:use %s....)\n"
+          "  (:use [ring.util.servlet :only [defservice]]))\n"
+          "\n"
+          ";; Replace the ... below with the var containing your appengine-app.\n"
+          "(defservice ...)\n"))
+
+
+(defn dash_ [s]
+  (.replaceAll s "-" "_"))
+
+
+(defn _dash [s]
+  (.replaceAll s "_" "-"))
+
+
+(defn xpath-replace-all [input out-file expr-map]
   (let [input (if (instance? String input) (File. input) input)
         out-file (if (instance? String out-file) (File. out-file) out-file)
         in-stream (if (instance? InputStream input) input (FileInputStream. input))
         input-source (InputSource. in-stream)
         doc (.parse (.. (DocumentBuilderFactory/newInstance) newDocumentBuilder) input-source)
-        xpath (.newXPath (XPathFactory/newInstance))
-        xpath-expr (.compile xpath xpath-raw-expr)
-        xpath-expr-nodes (.evaluate xpath-expr doc XPathConstants/NODESET)]
-    (dotimes [i (.getLength xpath-expr-nodes)]
-      (.setTextContent (.item xpath-expr-nodes i) new-value))
+        xpath (.newXPath (XPathFactory/newInstance))]
+    (doseq [[xpath-raw-expr new-value] expr-map]
+      (let [xpath-expr (.compile xpath xpath-raw-expr)
+            xpath-expr-nodes (.evaluate xpath-expr doc XPathConstants/NODESET)]
+        (dotimes [i (.getLength xpath-expr-nodes)]
+          (.setTextContent (.item xpath-expr-nodes i) new-value))))
     (let [source (DOMSource. doc)
           result (StreamResult. out-file)
           transformer (.newTransformer (TransformerFactory/newInstance))]
@@ -31,8 +51,9 @@
         war-dir (File. resources-dir "war")
         WEB-INF-dir (File. war-dir "WEB-INF")
         prj-application (:appengine-application project)
-        prj-display-name (:appengine-display-name project)]
-    (println "making new App Engine project")
+        prj-display-name (:appengine-display-name project)
+        prj-servlet (:appengine-servlet project "app_servlet")]
+    (println "making a skeleton for a Google App Engine application")
     ;; verify required entries
     (when-not prj-application
       (println ":appengine-application required in project.clj")
@@ -50,6 +71,20 @@
     (when-not (.exists WEB-INF-dir)
       (.mkdir WEB-INF-dir)
       (println "created WEB-INF directory" (.getPath WEB-INF-dir)))
+    ;; write a base entry point servlet file
+    (let [src-dir (File. (:source-path project))
+          src-base-namespace-dir (File. src-dir (dash_ prj-application))
+          entry-servlet-file (File. src-base-namespace-dir (str prj-servlet ".clj"))]
+      (when-not (.exists src-base-namespace-dir)
+        (.mkdir src-base-namespace-dir)
+        (println "created source base namespace directory" (.getPath src-base-namespace-dir)))
+      (when-not (.exists entry-servlet-file)
+        (with-open [writer (FileWriter. entry-servlet-file)]
+          (.write writer (format app-servlet-src
+                                 (_dash prj-application)
+                                 prj-servlet
+                                 (_dash prj-application))))
+        (println "created base entry point servlet" (.getPath entry-servlet-file))))
     ;; add required configuration files
     (let [in-web-xml (-> (clojure.lang.RT/baseLoader)
                          (.getResourceAsStream "web.xml"))
@@ -59,13 +94,16 @@
           out-appengine-web-xml (File. WEB-INF-dir "appengine-web.xml")]
       (if (.exists out-web-xml)
           (println out-web-xml "already exists, not overwriting")
-          (do (xpath-replace-all in-web-xml out-web-xml "//display-name" prj-display-name)
+          (do (xpath-replace-all in-web-xml out-web-xml
+                                 {"//display-name" prj-display-name
+                                  "//servlet-class" (str (dash_ prj-application)
+                                                         "."
+                                                         (dash_ prj-servlet))})
               (println "web.xml written to" (.getPath out-web-xml))))
       (if (.exists out-appengine-web-xml)
           (println out-appengine-web-xml "already exists, not overwriting")
           (do (xpath-replace-all in-appengine-web-xml out-appengine-web-xml
-                                 "//application" prj-application)
+                                 {"//application" prj-application})
               (println "appengine-web.xml written to" (.getPath out-appengine-web-xml)))))
     ;; done
-    (println "App Engine project" prj-display-name
-             "created; you should add it to source control")))
+    (println "Google App Engine skeleton for" prj-display-name "created")))
