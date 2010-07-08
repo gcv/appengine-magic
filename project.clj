bd69e84d751f22293352f75f14ea94ecf1f5db82
Constantine Vetoshev <gepardcv@gmail.com>
2010-07-07 23:21:50 -0400

    Cleaned up dependencies file.

--- project.clj
+++ project.clj
@@ -2,15 +2,14 @@
   :description "Google App Engine library for Clojure."
   :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
   :dependencies [[org.clojure/clojure "1.1.0"]
+                 [ring/ring-core "0.2.5"]
+                 [ring/ring-servlet "0.2.5"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]
-                 [ring/ring-core "0.2.2"]
-                 [ring/ring-servlet "0.2.1"]]
-  ;; TODO: Some of these may not be necessary.
-  :dev-dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
-                     [com.google.appengine/appengine-api-labs "1.3.4"]
-                     [com.google.appengine/appengine-api-stubs "1.3.4"]
-                     [com.google.appengine/appengine-local-runtime "1.3.4"]
-                     [com.google.appengine/appengine-testing "1.3.4"]
-                     [org.mortbay.jetty/jetty "6.1.21"]
-                     [swank-clojure "1.2.1"]])
+                 [org.mortbay.jetty/jetty "6.1.21"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
+                 [com.google.appengine/appengine-api-labs "1.3.4"]
+                 [com.google.appengine/appengine-api-stubs "1.3.4"]
+                 [com.google.appengine/appengine-local-runtime "1.3.4"]
+                 [com.google.appengine/appengine-testing "1.3.4"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
