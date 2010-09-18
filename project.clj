367bba80178cc37744aba3ee4557fb37799af800
Constantine Vetoshev <gepardcv@gmail.com>
2010-09-18 15:09:52 -0400

    Fixed dependencies.

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject appengine-magic "0.1.0-SNAPSHOT"
   :description "Google App Engine library for Clojure."
   :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [ring/ring-core "0.2.5"]
-                 [ring/ring-servlet "0.2.5"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [ring/ring-core "0.3.0-RC2"]
+                 [ring/ring-servlet "0.3.0-RC2"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]
                  [org.mortbay.jetty/jetty "6.1.21"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
-                 [com.google.appengine/appengine-api-labs "1.3.4"]
-                 [com.google.appengine/appengine-api-stubs "1.3.4"]
-                 [com.google.appengine/appengine-local-runtime "1.3.4"]
-                 [com.google.appengine/appengine-testing "1.3.4"]]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.3.7"]
+                 [com.google.appengine/appengine-api-labs "1.3.7"]
+                 [com.google.appengine/appengine-api-stubs "1.3.7"]
+                 [com.google.appengine/appengine-local-runtime "1.3.7"]
+                 [com.google.appengine/appengine-testing "1.3.7"]
+                 [com.google.appengine/appengine-tools-api "1.3.7"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
