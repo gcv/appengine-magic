6d8b93e9ea9a2a4d3c730763465f9f9276db2841
Constantine Vetoshev <gepardcv@gmail.com>
2010-09-18 15:09:21 -0400

    Giving up on dedicated Leiningen tasks to update and rollback App Engine applications. Seems impossible without a full App Engine environment, and in that case, the appcfg.sh script works better. Replaced all that with a Leiningen task which prepares the "war" directory for deployment. Does not quite work yet, class loading problems.

---
+++ src/leiningen/appengine_prepare.clj
@@ -0,0 +1,39 @@
+(ns leiningen.appengine-prepare
+  "Prepares a the Google App Engine application for deployment."
+  (:use appengine-magic.utils)
+  (:require leiningen.compile lancet)
+  (:import java.io.File))
+
+
+(defn appengine-prepare [project]
+  (println "preparing App Engine application" (:appengine-display-name project)
+           "for deployment")
+  (let [classes-dir *compile-path*
+        resources-dir (File. (:resources-path project))
+        lib-dir (File. (:library-path project))
+        lib-dev-dir (File. lib-dir "dev")
+        war-dir (File. resources-dir "war")
+        WEB-INF-dir (File. war-dir "WEB-INF")
+        target-classes-dir (File. WEB-INF-dir "classes")
+        target-lib-dir (File. WEB-INF-dir "lib")
+        prj-application (:appengine-application project)]
+    ;; step 1: compile all
+    (leiningen.compile/compile project)
+    ;; TODO: Delete existing content of target classes/ and lib/.
+    ;; step 2: copy the compiled app itself (only its namespace)
+    (lancet/mkdir {:dir target-classes-dir})
+    (lancet/mkdir {:dir target-lib-dir})
+    (lancet/copy {:todir (.getPath target-classes-dir)}
+                 (lancet/fileset
+                  {:dir *compile-path*
+                   :includes (str (dash_ prj-application) "/**")}))
+    ;; step 3: copy important dependencies into WEB-INF/lib
+    (lancet/copy {:todir (.getPath target-lib-dir)}
+                 (lancet/fileset
+                  {:dir lib-dir
+                   :includes "*"
+                   :excludes "dev"}))
+    (lancet/copy {:todir (.getPath target-lib-dir)}
+                 (lancet/fileset
+                  {:dir lib-dev-dir
+                   :includes "appengine-magic*,appengine-api-1.0-sdk*,appengine-api-labs*"}))))
--- src/leiningen/appengine_rollback.clj
+++
@@ -1,8 +0,0 @@
-(ns leiningen.appengine-rollback
-  "Roll the current Google App Engine application to a previous version.")
-
-
-(defn appengine-rollback [project]
-  (println "rolling back project")
-  ;; TODO: Fill this in.
-  )
