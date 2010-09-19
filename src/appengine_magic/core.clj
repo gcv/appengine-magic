a6e0b2b66e705958616b88174b5c6a38bc3f64b5
Constantine Vetoshev <gepardcv@gmail.com>
2010-09-19 09:40:03 -0400

    Working installation dispatch for deployed and interactive application modes.

--- src/appengine_magic/core.clj
+++ src/appengine_magic/core.clj
@@ -1,10 +1,14 @@
 (ns appengine-magic.core)
 
 
-(defn- in-appengine-dev-mode? []
-  false)
+(defn- in-appengine-interactive-mode? []
+  (try
+    (let [stack-trace (.getStackTrace (Thread/currentThread))]
+      (some #(.contains (.toString %) "swank.core") stack-trace))
+    (catch java.security.AccessControlException ace
+      false)))
 
 
-(if (in-appengine-dev-mode?)
+(if (in-appengine-interactive-mode?)
     (load "core_local")
     (load "core_google"))
