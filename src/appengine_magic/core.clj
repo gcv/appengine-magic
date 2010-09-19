ebf072e8004c1a7372e856228830ba7ed3be8695
Constantine Vetoshev <gepardcv@gmail.com>
2010-09-19 15:05:07 -0400

    Added interactive mode support for people just using a REPL, without SLIME.

--- src/appengine_magic/core.clj
+++ src/appengine_magic/core.clj
@@ -4,7 +4,9 @@
 (defn- in-appengine-interactive-mode? []
   (try
     (let [stack-trace (.getStackTrace (Thread/currentThread))]
-      (some #(.contains (.toString %) "swank.core") stack-trace))
+      (some #(or (.contains (.toString %) "swank.core")
+                 (.contains (.toString %) "clojure.main$repl"))
+            stack-trace))
     (catch java.security.AccessControlException ace
       false)))
 
