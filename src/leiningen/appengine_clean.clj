baa7ed740eeff86eb28b1f794b152b375977c1c8
Constantine Vetoshev <gepardcv@gmail.com>
2010-09-22 13:21:10 -0400

    Fixed small bug in appengine-clean Leiningen task.

--- src/leiningen/appengine_clean.clj
+++ src/leiningen/appengine_clean.clj
@@ -5,12 +5,13 @@
 
 
 (defn appengine-clean [project]
-  (println "cleaning out App Engine application" (:appengine-display-name project))
-  (let [resources-dir (File. (:resources-path project))
+  (let [prj-application (or (:appengine-application project) (:name project))
+        resources-dir (File. (:resources-path project))
         war-dir (File. resources-dir "war")
         WEB-INF-dir (File. war-dir "WEB-INF")
         target-classes-dir (File. WEB-INF-dir "classes")
         target-lib-dir (File. WEB-INF-dir "lib")]
+    (println "cleaning out App Engine application" prj-application)
     ;; delete existing content of target classes/ and lib/
     (lancet/delete {:dir (.getPath target-classes-dir)})
     (lancet/delete {:dir (.getPath target-lib-dir)})))
