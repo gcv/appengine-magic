95865eb22232ea20b10bab005700df40c70d4132
Constantine Vetoshev <gepardcv@gmail.com>
2010-10-22 18:07:50 -0400

    Bumped up version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject appengine-magic "0.2.1"
+(defproject appengine-magic "0.2.2-SNAPSHOT"
   :description "Google App Engine library for Clojure."
   :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
   :dependencies [[org.clojure/clojure "1.2.0"]
