eb1c4a33ae61362ec17d7d2de8038495e1f85f09
Constantine Vetoshev <gepardcv@gmail.com>
2010-11-12 15:22:36 -0500

    First cut of parsing incoming messages.

--- src/appengine_magic/services/mail.clj
+++ src/appengine_magic/services/mail.clj
@@ -1,5 +1,6 @@
 (ns appengine-magic.services.mail
   (:refer-clojure :exclude (send))
+  (:use [appengine-magic.utils :only [record]])
   (:import [com.google.appengine.api.mail MailServiceFactory
             MailService$Message MailService$Attachment]))
 
@@ -14,16 +15,17 @@
 
 
 (defrecord MailMessage [from to subject text-body html-body
-                        reply-to cc bcc attachments])
+                        sent-date received-date
+                        reply-to cc bcc attachments message-id])
 
 
 (defn make-attachment [filename data]
   (MailService$Attachment. filename data))
 
 
-(defn make-mail-message [& {:keys [from to subject text-body html-body
-                                   reply-to cc bcc attachments]
-                            :or {text-body "", html-body "", cc [], bcc [], attachments []}}]
+(defn make-message [& {:keys [from to subject text-body html-body
+                              reply-to cc bcc attachments]
+                       :or {text-body "", html-body "", cc [], bcc [], attachments []}}]
   ;; normalize and error-check
   (let [from (if-not (nil? from)
                  from
@@ -41,7 +43,11 @@
                     (throw (IllegalArgumentException. ":subject argument required")))
         cc (if (not (or (nil? cc) (sequential? cc))) [cc] cc)
         bcc (if (not (or (nil? bcc) (sequential? bcc))) [bcc] bcc)]
-    (MailMessage. from to subject text-body html-body reply-to cc bcc attachments)))
+    (record MailMessage
+            :from from :to to :subject subject
+            :text-body text-body :html-body html-body
+            :reply-to reply-to :cc cc :bcc bcc
+            :attachments attachments)))
 
 
 (defn- make-mail-service-message [^MailMessage msg]
@@ -63,3 +69,19 @@
 
 (defn send-to-admins [^MailMessage msg]
   (.sendToAdmins (get-mail-service) (make-mail-service-message msg)))
+
+
+(defn parse-message [request]
+  (let [session (javax.mail.Session/getDefaultInstance (java.util.Properties.) nil)
+        raw-msg (javax.mail.internet.MimeMessage. session (.getInputStream (:request request)))
+        from (first (map #(.toString %) (.getFrom raw-msg)))
+        subject (.getSubject raw-msg)
+        sent-date (.getSentDate raw-msg)
+        received-date (.getReceivedDate raw-msg)
+        to (map #(.toString %) (.getRecipients raw-msg javax.mail.Message$RecipientType/TO))
+        cc (map #(.toString %) (.getRecipients raw-msg javax.mail.Message$RecipientType/CC))
+        reply-to (first (map #(.toString %) (.getReplyTo raw-msg)))
+        message-id (.getMessageID raw-msg)]
+    (record MailMessage
+            :from from :to to :subject subject
+            :reply-to reply-to :cc cc)))
