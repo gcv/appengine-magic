d26cf3d1f4015d219411322c1b1360d7be43ff9c
Constantine Vetoshev <gepardcv@gmail.com>
2010-11-14 17:04:01 -0500

    Added support for incoming mail.

--- src/appengine_magic/services/mail.clj
+++ src/appengine_magic/services/mail.clj
@@ -1,6 +1,6 @@
 (ns appengine-magic.services.mail
   (:refer-clojure :exclude (send))
-  (:use [appengine-magic.utils :only [record]])
+  (:use [appengine-magic.utils :only [record copy-stream]])
   (:import [com.google.appengine.api.mail MailServiceFactory
             MailService$Message MailService$Attachment]))
 
@@ -20,7 +20,9 @@
 
 
 (defn make-attachment [filename data]
-  (MailService$Attachment. filename data))
+  (MailService$Attachment. filename (if (instance? (class (.getBytes "")) data)
+                                        data
+                                        (.getBytes data))))
 
 
 (defn make-message [& {:keys [from to subject text-body html-body
@@ -71,6 +73,34 @@
   (.sendToAdmins (get-mail-service) (make-mail-service-message msg)))
 
 
+(defrecord #^{:private true} MessageAlternativeParts [parts])
+(defrecord #^{:private true} MessagePart [filename content-type data])
+
+
+(defn- deconstruct-message [#^MimeMessage message]
+  (let [all-subparts (fn [part]
+                       (map #(.getBodyPart part %) (range (.getCount part))))
+        subparts (fn subparts [part]
+                   (let [content-type (.toLowerCase (.getContentType part))]
+                     (cond
+                      ;; multiple attachments
+                      (re-matches #"multipart\/mixed.*" content-type)
+                      (doall (map subparts (all-subparts (.getContent part))))
+                      ;; probably message text
+                      (re-matches #"multipart\/alternative.*" content-type)
+                      (MessageAlternativeParts.
+                       (doall (map subparts (all-subparts (.getContent part)))))
+                      ;; a specific attachment
+                      :else (let [content (.getContent part)]
+                              (if (string? content)
+                                  (MessagePart. (.getFileName part) content-type content)
+                                  (with-open [tempout (java.io.ByteArrayOutputStream.)]
+                                    (copy-stream (.getInputStream part) tempout)
+                                    (MessagePart. (.getFileName part) content-type
+                                                  (.toByteArray tempout))))))))]
+    (flatten [(subparts message)])))
+
+
 (defn parse-message [request]
   (let [session (javax.mail.Session/getDefaultInstance (java.util.Properties.) nil)
         raw-msg (javax.mail.internet.MimeMessage. session (.getInputStream (:request request)))
@@ -81,7 +111,30 @@
         to (map #(.toString %) (.getRecipients raw-msg javax.mail.Message$RecipientType/TO))
         cc (map #(.toString %) (.getRecipients raw-msg javax.mail.Message$RecipientType/CC))
         reply-to (first (map #(.toString %) (.getReplyTo raw-msg)))
-        message-id (.getMessageID raw-msg)]
+        message-id (.getMessageID raw-msg)
+        msg-raw-parts (deconstruct-message raw-msg)
+        msg-parts (reduce (fn breakdown [acc raw-part]
+                            (cond
+                             ;; alternatives
+                             (instance? MessageAlternativeParts raw-part)
+                             (reduce breakdown acc (:parts raw-part))
+                             ;; has a valid filename, must be an attachment
+                             (not (nil? (:filename raw-part)))
+                             (assoc acc :attachments
+                                    (conj (:attachments acc)
+                                          (make-attachment (:filename raw-part)
+                                                           (:data raw-part))))
+                             ;; text part
+                             (re-matches #"text\/plain.*" (:content-type raw-part))
+                             (assoc acc :text-body (:data raw-part))
+                             ;; HTML part
+                             (re-matches #"text\/html.*" (:content-type raw-part))
+                             (assoc acc :html-body (:data raw-part))))
+                          {:attachments []}
+                          msg-raw-parts)]
     (record MailMessage
             :from from :to to :subject subject
-            :reply-to reply-to :cc cc)))
+            :reply-to reply-to :cc cc
+            :text-body (:text-body msg-parts) :html-body (:html-body msg-parts)
+            :attachments (:attachments msg-parts)
+            :message-id message-id)))
