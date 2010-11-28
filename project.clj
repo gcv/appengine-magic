1c79ddb77c07a4494cc259d20f4533024cac73a9
Constantine Vetoshev <gepardcv@gmail.com>
2010-11-28 18:15:54 -0500

    Updated dependencies for the administrative console.

--- project.clj
+++ project.clj
@@ -4,12 +4,19 @@
                  "snapshots" "https://github.com/gcv/maven-repository/raw/master/snapshots/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [ring/ring-core "0.3.5"]
+                 [org.apache.commons/commons-exec "1.1"]
+                 ;; App Engine supporting essentials
                  [javax.servlet/servlet-api "2.5"]
                  [commons-io "1.4"]
                  [commons-codec "1.4"]
                  [commons-fileupload "1.2.1"]
+                 ;; App Engine administrative interface support
+                 [tomcat/jasper-runtime "5.0.28"]
+                 [org.apache.geronimo.specs/geronimo-jsp_2.1_spec "1.0.1"]
+                 [jstl "1.1.2"] ; repackaged-appengine-jakarta-jstl-1.1.2.jar
+                 [taglibs/standard "1.1.2"] ; repackaged-appengine-jakarta-standard-1.1.2.jar
                  [commons-el "1.0"]
-                 [org.apache.commons/commons-exec "1.1"]
+                 ;; main App Engine libraries
                  [com.google.appengine/appengine-api-1.0-sdk "1.3.8"]
                  [com.google.appengine/appengine-api-labs "1.3.8"]
                  [com.google.appengine/appengine-api-stubs "1.3.8"]
