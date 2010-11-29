df2561b30447b8f7c812afef994fd280609ef2d4
Constantine Vetoshev <gepardcv@gmail.com>
2010-11-28 20:48:13 -0500

    Cleaned up appengine-magic.core/start and stop functions; implemented a serve function which tries to do the right thing to start and restart intelligently.

--- src/appengine_magic/core_local.clj
+++ src/appengine_magic/core_local.clj
@@ -8,9 +8,14 @@
          '[appengine-magic.jetty :as jetty]
          '[appengine-magic.blobstore-upload :as blobstore-upload])
 
-(import java.io.File)
+(import java.io.File
+        com.google.apphosting.api.ApiProxy)
 
-(import com.google.apphosting.api.ApiProxy)
+
+
+;;; ----------------------------------------------------------------------------
+;;; appengine-magic core API functions
+;;; ----------------------------------------------------------------------------
 
 (defn default-war-root []
   (-> (clojure.lang.RT/baseLoader)
@@ -63,61 +68,67 @@
       (.doFilter chain req resp))))
 
 
-(defn start* [appengine-app & {:keys [port join?]}]
+
+;;; ----------------------------------------------------------------------------
+;;; development server controls
+;;; ----------------------------------------------------------------------------
+
+(defonce *server* (atom nil))
+
+
+(defn start [appengine-app & {:keys [port join?] :or {port 8080, join? false}}]
   (let [war-root (java.io.File. (:war-root appengine-app))
         handler-servlet (servlet (:handler appengine-app))]
     (appengine-init war-root port)
-    (jetty/start
-     {"/*" [(make-appengine-request-environment-filter)
-            (com.google.apphosting.utils.servlet.TransactionCleanupFilter.)
-            (com.google.appengine.api.blobstore.dev.ServeBlobFilter.)]}
-     {"/" handler-servlet
-      "/_ah/login" (com.google.appengine.api.users.dev.LocalLoginServlet.)
-      "/_ah/logout" (com.google.appengine.api.users.dev.LocalLogoutServlet.)
-      "/_ah/upload" (servlet (blobstore-upload/make-blob-upload-handler war-root))
-      ;;"/_ah/img" (com.google.appengine.api.images.dev.LocalBlobImageServlet.)
-      "/_ah/channel/jsapi" (com.google.appengine.api.channel.dev.ServeScriptServlet.)
-      "/_ah/channel/dev" (com.google.appengine.api.channel.dev.LocalChannelServlet.)
-      ;; These mappings are from webdefault.xml in appengine-local-runtime-*.jar.
-      "/_ah/sessioncleanup" (com.google.apphosting.utils.servlet.SessionCleanupServlet.)
-      "/_ah/admin" (com.google.apphosting.utils.servlet.DatastoreViewerServlet.)
-      "/_ah/admin/" (com.google.apphosting.utils.servlet.DatastoreViewerServlet.)
-      "/_ah/admin/datastore" (com.google.apphosting.utils.servlet.DatastoreViewerServlet.)
-      "/_ah/admin/taskqueue" (com.google.apphosting.utils.servlet.TaskQueueViewerServlet.)
-      "/_ah/admin/xmpp" (com.google.apphosting.utils.servlet.XmppServlet.)
-      "/_ah/admin/inboundmail" (com.google.apphosting.utils.servlet.InboundMailServlet.)
-      "/_ah/resources" (com.google.apphosting.utils.servlet.AdminConsoleResourceServlet.)
-      "/_ah/adminConsole" (org.apache.jsp.ah.adminConsole_jsp.)
-      "/_ah/datastoreViewerHead" (org.apache.jsp.ah.datastoreViewerHead_jsp.)
-      "/_ah/datastoreViewerBody" (org.apache.jsp.ah.datastoreViewerBody_jsp.)
-      "/_ah/datastoreViewerFinal" (org.apache.jsp.ah.datastoreViewerFinal_jsp.)
-      "/_ah/entityDetailsHead" (org.apache.jsp.ah.entityDetailsHead_jsp.)
-      "/_ah/entityDetailsBody" (org.apache.jsp.ah.entityDetailsBody_jsp.)
-      "/_ah/entityDetailsFinal" (org.apache.jsp.ah.entityDetailsFinal_jsp.)
-      "/_ah/taskqueueViewerHead" (org.apache.jsp.ah.taskqueueViewerHead_jsp.)
-      "/_ah/taskqueueViewerBody" (org.apache.jsp.ah.taskqueueViewerBody_jsp.)
-      "/_ah/taskqueueViewerFinal" (org.apache.jsp.ah.taskqueueViewerFinal_jsp.)
-      "/_ah/xmppHead" (org.apache.jsp.ah.xmppHead_jsp.)
-      "/_ah/xmppBody" (org.apache.jsp.ah.xmppBody_jsp.)
-      "/_ah/xmppFinal" (org.apache.jsp.ah.xmppFinal_jsp.)
-      "/_ah/inboundmailHead" (org.apache.jsp.ah.inboundMailHead_jsp.)
-      "/_ah/inboundmailBody" (org.apache.jsp.ah.inboundMailBody_jsp.)
-      "/_ah/inboundmailFinal" (org.apache.jsp.ah.inboundMailFinal_jsp.)}
-     :port port
-     :join? join?)))
+    (reset!
+     *server*
+     (jetty/start
+      {"/*" [(make-appengine-request-environment-filter)
+             (com.google.apphosting.utils.servlet.TransactionCleanupFilter.)
+             (com.google.appengine.api.blobstore.dev.ServeBlobFilter.)]}
+      {"/" handler-servlet
+       "/_ah/login" (com.google.appengine.api.users.dev.LocalLoginServlet.)
+       "/_ah/logout" (com.google.appengine.api.users.dev.LocalLogoutServlet.)
+       "/_ah/upload" (servlet (blobstore-upload/make-blob-upload-handler war-root))
+       ;;"/_ah/img" (com.google.appengine.api.images.dev.LocalBlobImageServlet.)
+       "/_ah/channel/jsapi" (com.google.appengine.api.channel.dev.ServeScriptServlet.)
+       "/_ah/channel/dev" (com.google.appengine.api.channel.dev.LocalChannelServlet.)
+       ;; These mappings are from webdefault.xml in appengine-local-runtime-*.jar.
+       "/_ah/sessioncleanup" (com.google.apphosting.utils.servlet.SessionCleanupServlet.)
+       "/_ah/admin" (com.google.apphosting.utils.servlet.DatastoreViewerServlet.)
+       "/_ah/admin/" (com.google.apphosting.utils.servlet.DatastoreViewerServlet.)
+       "/_ah/admin/datastore" (com.google.apphosting.utils.servlet.DatastoreViewerServlet.)
+       "/_ah/admin/taskqueue" (com.google.apphosting.utils.servlet.TaskQueueViewerServlet.)
+       "/_ah/admin/xmpp" (com.google.apphosting.utils.servlet.XmppServlet.)
+       "/_ah/admin/inboundmail" (com.google.apphosting.utils.servlet.InboundMailServlet.)
+       "/_ah/resources" (com.google.apphosting.utils.servlet.AdminConsoleResourceServlet.)
+       "/_ah/adminConsole" (org.apache.jsp.ah.adminConsole_jsp.)
+       "/_ah/datastoreViewerHead" (org.apache.jsp.ah.datastoreViewerHead_jsp.)
+       "/_ah/datastoreViewerBody" (org.apache.jsp.ah.datastoreViewerBody_jsp.)
+       "/_ah/datastoreViewerFinal" (org.apache.jsp.ah.datastoreViewerFinal_jsp.)
+       "/_ah/entityDetailsHead" (org.apache.jsp.ah.entityDetailsHead_jsp.)
+       "/_ah/entityDetailsBody" (org.apache.jsp.ah.entityDetailsBody_jsp.)
+       "/_ah/entityDetailsFinal" (org.apache.jsp.ah.entityDetailsFinal_jsp.)
+       "/_ah/taskqueueViewerHead" (org.apache.jsp.ah.taskqueueViewerHead_jsp.)
+       "/_ah/taskqueueViewerBody" (org.apache.jsp.ah.taskqueueViewerBody_jsp.)
+       "/_ah/taskqueueViewerFinal" (org.apache.jsp.ah.taskqueueViewerFinal_jsp.)
+       "/_ah/xmppHead" (org.apache.jsp.ah.xmppHead_jsp.)
+       "/_ah/xmppBody" (org.apache.jsp.ah.xmppBody_jsp.)
+       "/_ah/xmppFinal" (org.apache.jsp.ah.xmppFinal_jsp.)
+       "/_ah/inboundmailHead" (org.apache.jsp.ah.inboundMailHead_jsp.)
+       "/_ah/inboundmailBody" (org.apache.jsp.ah.inboundMailBody_jsp.)
+       "/_ah/inboundmailFinal" (org.apache.jsp.ah.inboundMailFinal_jsp.)}
+      :port port
+      :join? join?))))
 
 
-(defn stop* [server]
-  (appengine-clear)
-  (jetty/stop server))
+(defn stop []
+  (when-not (nil? @*server*)
+    (appengine-clear)
+    (jetty/stop @*server*)
+    (reset! *server* nil)))
 
 
-(defmacro start [appengine-app &
-                 {:keys [server port join?]
-                  :or {server '*server* port 8080 join? false}}]
-  `(do (defonce ~server (atom nil))
-       (reset! ~server (start* ~appengine-app :port ~port :join? ~join?))))
-
-
-(defmacro stop [& {:keys [server] :or {server '*server*}}]
-  `(stop* @~server))
+(defn serve [appengine-app & {:keys [port] :or {port 8080}}]
+  (stop)
+  (start appengine-app :port port))
