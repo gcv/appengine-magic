c2cf0d5ea2e21f6a22adc16fb2a0cb685926d49c
Masashi Iizuka <liquidz.uo@gmail.com>
2011-02-07 21:42:26 +0900

    Added testing support for the User service.

--- src/appengine_magic/services/user.clj
+++ src/appengine_magic/services/user.clj
@@ -31,3 +31,12 @@
 (defn logout-url [& {:keys [destination]
                      :or {destination "/"}}]
   (.createLogoutURL (get-user-service) destination))
+
+(defn get-email [#^User user]
+  (.getEmail user))
+
+(defn get-nickname [#^User user]
+  (.getNickname user))
+
+(defn get-auth-domain [#^User user]
+  (.getAuthDomain user))
